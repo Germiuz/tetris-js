73cf7a38b17b330a06e1e2dd556af97d5c882373
Aleksey <germius@yandex.ru>
2024-03-28 17:19:44 +0500

    Marked all Game class internal method as private. Added shadow block (that display where current block will be dropped)

--- src/pages/GameField/Components/Cell/Cell.tsx
+++ src/pages/GameField/Components/Cell/Cell.tsx
@@ -5,8 +5,29 @@ import styles from './Cell.module.css';
 
 const CellColors = [styles.cellTypeJ, styles.cellTypeL, styles.cellTypeI, styles.cellTypeT, styles.cellTypeZ, styles.cellTypeS, styles.cellTypeO];
 
-const Symbols = ['', '🌷', '🌸', '🌹', '🌺', '🌻', '🌼', '💐', '💮', '🏵️']
+const Symbols = ['🌷', '🌸', '🌹', '🌺', '🌻', '🌼', '💐', '💮', '🏵️']
 // const Symbols = ['', '❀', '✿', '❁', '⚘', '❃', '🏵', '⚜', '✾']
+
+const getSymbol = (colorId: number) => {
+    if (colorId > 0) {
+        return Symbols[colorId - 1]
+    }
+    return '';
+}
+
+const getCellClass = (colorId: number) => {
+    if (colorId > 0) {
+        return [styles.cellBlock, CellColors[colorId - 1]]
+    }
+
+    if (colorId === 0) {
+        return styles.cellEmpty;
+    }
+
+    if (colorId < 0) {
+        return [styles.cellBlock, styles.cellShadow];
+    }
+}
 
 export const Cell: React.FC<{
     readonly colorId: number;
@@ -15,10 +36,9 @@ export const Cell: React.FC<{
     colorId,
     className
 }) => {
-    const cellClass = colorId === 0 ? styles.cellEmpty : [styles.cellBlock, CellColors[colorId - 1]];
     return (
-        <div className={classNames(styles.cell, cellClass, className)}>
-            <span className={styles.symbol}>{Symbols[colorId]}</span>
+        <div className={classNames(styles.cell, getCellClass(colorId), className)}>
+            <span className={styles.symbol}>{getSymbol(colorId)}</span>
         </div>
     )
 }
--- src/store/game/Game.ts
+++ src/store/game/Game.ts
@@ -39,10 +39,12 @@ export class BlockTypeGenerator {
 }
 
 export class Game {
-    private cells: number[][] = [];
+    private _cells: number[][] = [];
 
     private readonly _rows: number;
     private readonly _cols: number;
+
+    private readonly _withShadowBlock: boolean = true;
 
     private _score: number = 0;
     private _totalLines: number = 0;
@@ -96,7 +98,7 @@ export class Game {
     }
 
     private moveBlockBy(xOffset: number, yOffset: number) {
-        if (this.currentBlock && this.checkBlockPosition(xOffset, yOffset)) {
+        if (this.currentBlock && this.checkBlockCanMove(xOffset, yOffset)) {
             this.currentBlock.pos.x += xOffset;
             this.currentBlock.pos.y += yOffset;
         }
@@ -123,7 +125,7 @@ export class Game {
     }
 
     private removeCompletedLines() {
-        const uncompletedLines: number[][] = this.cells.filter((row) =>
+        const uncompletedLines: number[][] = this._cells.filter((row) =>
             row.some((cell) => cell === 0)
         );
 
@@ -132,12 +134,69 @@ export class Game {
 
         if (completedCnt > 0) {
             const addLines: number[][] = getEmptyMatrix(completedCnt, this.cols);
-            this.cells = [...addLines, ...uncompletedLines];
+            this._cells = [...addLines, ...uncompletedLines];
+        }
+    }
+
+    private checkBlockCanMove(xOffset: number, yOffset: number): boolean {
+        if (!this.currentBlock) {
+            return false;
+        }
+
+        const blockPoints = this.getBlockPoints();
+
+        const collisions = blockPoints
+            .map(({x, y}) => ({
+                x: x + xOffset,
+                y: y + yOffset
+            }))
+            .filter(({y}) => y >= 0)
+            .filter(({x, y}) => x >= this._cols || x < 0 || y >= this._rows || this._cells[y][x] != 0);
+
+        return collisions.length === 0;
+    }
+
+    private getBlockPoints(): BlockPoint[] {
+        if (!this.currentBlock) {
+            return [];
+        }
+
+        const pos = this.currentBlock.pos;
+
+        return this.currentBlock.block.getPoints().map(({x, y, color}) => ({
+            x: pos.x + x,
+            y: pos.y + y,
+            color,
+        }))
+    }
+
+    private getBlockCell(row: number, col: number) {
+        if (this.currentBlock) {
+            return this.currentBlock.block.getCell(col - this.currentBlock.pos.x, row - this.currentBlock.pos.y)
+        }
+        return 0;
+    }
+
+    private getShadowBlockCell(row: number, col: number) {
+        if (this.currentBlock) {
+            let yShift = 0;
+            while (this.checkBlockCanMove(0, yShift + 1)) {
+                yShift++;
+            }
+
+            return this.currentBlock.block.getCell(col - this.currentBlock.pos.x, row - this.currentBlock.pos.y - yShift) > 0
+        }
+        return 0;
+    }
+
+    private setCell(row: number, col: number, val: number) {
+        if (row >= 0 && row < this._rows && col >= 0 && col < this._cols) {
+            this._cells[row][col] = val;
         }
     }
 
     startNewGame() {
-        this.cells = getEmptyMatrix(this._rows, this._cols);
+        this._cells = getEmptyMatrix(this._rows, this._cols);
 
         this._score = 0;
         this._totalLines = 0;
@@ -172,7 +231,7 @@ export class Game {
 
         this.newBlock();
 
-        if (!this.checkBlockPosition(0, 0)) {
+        if (!this.checkBlockCanMove(0, 0)) {
             this._state = 'Over';
             this.currentBlock = undefined;
         }
@@ -184,23 +243,23 @@ export class Game {
         }
 
         this.currentBlock.block.rotate(direction);
-        if (!this.checkBlockPosition(0, 0)) {
-            if (this.checkBlockPosition(1, 0)) {
+        if (!this.checkBlockCanMove(0, 0)) {
+            if (this.checkBlockCanMove(1, 0)) {
                 this.moveBlockBy(1, 0)
                 return;
             }
 
-            if (this.checkBlockPosition(-1, 0)) {
+            if (this.checkBlockCanMove(-1, 0)) {
                 this.moveBlockBy(-1, 0)
                 return;
             }
 
-            if (this.checkBlockPosition(2, 0)) {
+            if (this.checkBlockCanMove(2, 0)) {
                 this.moveBlockBy(2, 0)
                 return;
             }
 
-            if (this.checkBlockPosition(-2, 0)) {
+            if (this.checkBlockCanMove(-2, 0)) {
                 this.moveBlockBy(-2, 0)
                 return;
             }
@@ -213,17 +272,17 @@ export class Game {
         if (this.currentBlock) {
             switch (direction) {
                 case 'Left':
-                    if (this.checkBlockPosition(-1, 0)) {
+                    if (this.checkBlockCanMove(-1, 0)) {
                         this.moveBlockBy(-1, 0);
                     }
                     break;
                 case 'Right':
-                    if (this.checkBlockPosition(1, 0)) {
+                    if (this.checkBlockCanMove(1, 0)) {
                         this.moveBlockBy(1, 0);
                     }
                     break;
                 case 'Down':
-                    if (this.checkBlockPosition(0, 1)) {
+                    if (this.checkBlockCanMove(0, 1)) {
                         this.moveBlockBy(0, 1);
                     }
                     else {
@@ -235,57 +294,31 @@ export class Game {
     }
 
     downBlock() {
-        while (this.checkBlockPosition(0, 1)) {
+        while (this.checkBlockCanMove(0, 1)) {
             this.moveBlockBy(0, 1);
         }
-        //On last chance to move
+        //One last chance to move
         //this.nextBlock();
     }
 
-    checkBlockPosition(xOffset: number, yOffset: number): boolean {
-        if (!this.currentBlock) {
-            return false;
-        }
-
-        const blockPoints = this.getBlockPoints();
-
-        const collisions = blockPoints
-            .map(({x, y}) => ({
-                x: x + xOffset,
-                y: y + yOffset
-            }))
-            .filter(({y}) => y >= 0)
-            .filter(({x, y}) => x >= this._cols || x < 0 || y >= this._rows || this.cells[y][x] != 0);
-
-        return collisions.length === 0;
-    }
-
-    getBlockPoints(): BlockPoint[] {
-        if (!this.currentBlock) {
-            return [];
-        }
-
-        const pos = this.currentBlock.pos;
-
-        return this.currentBlock.block.getPoints().map(({x, y, color}) => ({
-            x: pos.x + x,
-            y: pos.y + y,
-            color,
-        }))
-    }
-
-    getBlockCell(row: number, col: number) {
-        if (this.currentBlock) {
-            return this.currentBlock.block.getCell(col - this.currentBlock.pos.x, row - this.currentBlock.pos.y)
-        }
-        return 0;
-    }
-
     getCell(row: number, col: number) {
-        return this.getBlockCell(row, col) || this.cells?.[row]?.[col] || 0;
-    }
+        const blockCell = this.getBlockCell(row, col);
+        if (blockCell) {
+            return blockCell;
+        }
 
-    setCell(row: number, col: number, val: number) {
-        this.cells[row][col] = val;
+        const gridCell = this._cells?.[row]?.[col];
+        if (gridCell) {
+            return gridCell;
+        }
+
+        if (this._withShadowBlock) {
+            const shadowBlockCell = this.getShadowBlockCell(row, col);
+            if (shadowBlockCell) {
+                return -1;
+            }
+        }
+
+        return 0;
     }
 }
